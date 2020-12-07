751b10a1bbff84ac7bc81caaea750a2868b1ed03
Evan Reid <evan_1995@hotmail.com>
2020-12-07 23:42:38 +0000

    ep 6 answered

---
+++ episode_6.js
@@ -0,0 +1,23 @@
+// episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// I think it's going to say "murderer is mrs white", the last value to be assigned to murderer was "mrs White" and at the end of changeMurderer plotTwist was called.
