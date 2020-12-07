062b07fdffff88eba5450848c8392436254785e8
Evan Reid <evan_1995@hotmail.com>
2020-12-07 23:31:22 +0000

    ep 5 answer

---
+++ episode_5.js
@@ -0,0 +1,21 @@
+// episode 5
+
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
+  
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
+  
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
+
+//   At first I thought there would be an error. But then it would seem that you can change the individual elements in the scenario object.
